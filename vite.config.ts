355171a05d4f0b4a03ecb0a24128c092a8d06172
windate3411 <windate3411@gmail.com>
2024-12-11 23:03:23 +0800

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts'; // 添加這行
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      include: ['src'],
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
